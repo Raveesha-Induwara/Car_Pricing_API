bebab960b429c232dbd0a0d01b53c32507a83f3b
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-02-03 19:22:14 +0530

    Excluding response properties

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from "typeorm";
-
+import { Exclude } from "class-transformer";
 @Entity()
 export class User {
 
@@ -10,6 +10,7 @@ export class User {
     @Column()
     email: string;
 
+    @Exclude()
     @Column()
     password: string;
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-users.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-users.dto';
@@ -15,9 +15,14 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get('/:id')
-    findUser(@Param('id') id: string){
-        return this.usersService.findOne(parseInt(id));
+    async findUser(@Param('id') id: string){
+        const user =  await this.usersService.findOne(parseInt(id));
+        if(!user) {
+            throw new NotFoundException('User not found');
+        }
+        return user;
     }
 
     @Get()
