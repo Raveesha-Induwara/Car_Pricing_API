0bdab8072315077ec6cc819540049dd5fe11590b
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-02-05 11:28:01 +0530

    Applly Serialize decorator to entier controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-users.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-users.dto';
@@ -7,6 +7,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
     constructor(private usersService: UsersService){}
@@ -17,11 +18,8 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    //@UseInterceptors(new SerializerInterceptor(UserDto))
-    @Serialize(UserDto) // custom decorator
     @Get('/:id')
     async findUser(@Param('id') id: string){
-        console.log('handler is running');
         const user =  await this.usersService.findOne(parseInt(id));
         if(!user) {
             throw new NotFoundException('User not found');
